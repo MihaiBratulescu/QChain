d9d383f8547e6eb88f8070455d9d6d29e0059464
Bratulescu Mihai <mihaibratulescu@outlook.com>
2026-04-26 13:17:54 +0300

    more tests

--- tests/Samples.OnlineShop.Tests/QChainIntegrationTestBench.cs
+++ tests/Samples.OnlineShop.Tests/QChainIntegrationTestBench.cs
@@ -1,5 +1,6 @@
 ﻿using QChain;
 using Samples.OnlineShop.Database;
+using Samples.OnlineShop.DatabaseModels;
 
 namespace Samples.OnlineShop.Tests;
 
@@ -9,6 +10,122 @@ public class QChainIntegrationTestBench(SqliteFixture fixture) : IClassFixture<S
 
     protected Task<T[]> Query<T>(Func<IUnitOfWork, IQuery<T>> query) =>
         _fixture.db.Query(query).ToArrayAsync(default);
+}
+
+public class Map(SqliteFixture fixture) : QChainIntegrationTestBench(fixture)
+{
+    [Fact]
+    public async Task SingleColumn()
+    {
+        var accounts = await Query(q => q.Accounts.Map(a => a.AccountId));
+        Assert.NotEmpty(accounts);
+    }
+
+    [Fact]
+    public async Task SingleEntity()
+    {
+        var accounts = await Query(q => q.Accounts.Map(a => new { a.AccountId, a.Name }));
+        Assert.NotEmpty(accounts);
+    }
+
+    [Fact]
+    public async Task Join()
+    {
+        var results = await Query(q =>
+            q.Accounts
+             .Join(q.Orders, a => a.AccountId, o => o.AccountId)
+             .Map(j => new { j.Item1.AccountId, orderAccountId = j.Item2.AccountId, j.Item2.OrderId }));
+
+        Assert.NotEmpty(results);
+        Assert.All(results, q => Assert.Equal(q.AccountId, q.orderAccountId));
+    }
+
+    [Fact]
+    public async Task GroupJoin()
+    {
+        var results = await Query(q =>
+            q.Accounts
+             .GroupJoin(q.Orders, a => a.AccountId, o => o.AccountId)
+             .Map(j => new 
+             { 
+                 j.Item1.AccountId, 
+                 orderIds = j.Item2.Select(o => ValueTuple.Create(o.OrderId, o.AccountId)) 
+             }));
+
+        Assert.NotEmpty(results);
+        foreach (var result in results)
+        {
+            Assert.All(result.orderIds, o => Assert.Equal(result.AccountId, o.Item2));
+        }
+    }
+}
+
+public class GroupJoin(SqliteFixture fixture) : QChainIntegrationTestBench(fixture)
+{
+    [Fact]
+    public async Task OnTable()
+    {
+        (string? name, IEnumerable<Account> accounts)[] result = await Query(q =>
+            q.Accounts.GroupBy(a => a.Name));
+
+        Assert.NotEmpty(result);
+        Assert.All(result, q => Assert.All(q.accounts, a => Assert.Equal(q.name, a.Name)));
+    }
+
+    [Fact]
+    public async Task WithProjection()
+    {
+        int[] result = await Query(q =>
+            q.Accounts.GroupBy(a => a.Name, a => a.Count()));
+
+        Assert.NotEmpty(result);
+        Assert.All(result, q => Assert.All(q.accounts, a => Assert.Equal(q.name, a.Name)));
+    }
+}
+
+public class Join(SqliteFixture fixture) : QChainIntegrationTestBench(fixture)
+{
+    [Fact]
+    public async Task TwoTables()
+    {
+        (Account account, Order order)[] result = await Query(q => 
+            q.Accounts
+             .Join(q.Orders, a => a.AccountId, o => o.AccountId));
+
+        Assert.NotEmpty(result);
+        Assert.All(result, q => Assert.Equal(q.account.AccountId, q.order.AccountId));
+    }
+
+    [Fact]
+    public async Task ThreeTables()
+    {
+        (Account account, Order order, Transaction transaction)[] result = await Query(q =>
+            q.Accounts
+             .Join(q.Orders, a => a.AccountId, o => o.AccountId)
+             .Join(q.Transactions, j => j.Item2.OrderId, t => t.OrderId, 
+                (j, t) => ValueTuple.Create(j.Item1, j.Item2, t)));
+
+        Assert.NotEmpty(result);
+        Assert.All(result, q =>
+        {
+            Assert.Equal(q.account.AccountId, q.order.AccountId);
+            Assert.Equal(q.order.OrderId, q.transaction.OrderId);
+        });
+    }
+
+    [Fact]
+    public async Task GroupJoin()
+    {
+        (Account account, IEnumerable<Order> orders)[] result = await Query(q =>
+            q.Accounts
+             .GroupJoin(q.Orders, a => a.AccountId, o => o.AccountId));
+
+        Assert.NotEmpty(result);
+        foreach (var (account, orders) in result)
+        {
+            Assert.All(orders, o => Assert.Equal(account.AccountId, o.AccountId));
+        }
+    }
 }
 
 public class Filtering(SqliteFixture fixture) : QChainIntegrationTestBench(fixture)
