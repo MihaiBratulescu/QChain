082ba6696bace6a6b366e050ec5f56ad5c17c801
Bratulescu Mihai <mihaibratulescu@outlook.com>
2026-05-17 15:07:52 +0300

    cccc

--- src/QChain.EntityFrameworkCore/QueryExecutor.cs
+++ src/QChain.EntityFrameworkCore/QueryExecutor.cs
@@ -63,7 +63,6 @@ internal class QueryExecutor<T> : IQueryExecutor<T>
     public Task<T?> SingleOrDefaultAsync(IQuery<T> query, Expression<Func<T, bool>> predicate, CancellationToken ct = default)
         => query.SingleOrDefaultAsync(predicate, ct);
 
-
     public Task<T[]> ToArrayAsync(IQuery<T> query, CancellationToken ct = default)
         => query.ToArrayAsync(ct);
 
@@ -72,4 +71,114 @@ internal class QueryExecutor<T> : IQueryExecutor<T>
 
     public string ToQueryString(IQuery<T> query)
         => query.ToQueryString();
+
+    public Task<T> LastAsync(IQuery<T> query, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> LastAsync(IQuery<T> query, Expression<Func<T, bool>> predicate, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> LastOrDefaultAsync(IQuery<T> query, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> LastOrDefaultAsync(IQuery<T> query, Expression<Func<T, bool>> predicate, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> MinAsync(IQuery<T> query, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> MaxAsync(IQuery<T> query, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal> SumAsync(IQuery<T> query, Expression<Func<T, decimal>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal?> SumAsync(IQuery<T> query, Expression<Func<T, decimal?>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int> SumAsync(IQuery<T> query, Expression<Func<T, int>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<int?> SumAsync(IQuery<T> query, Expression<Func<T, int?>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<long> SumAsync(IQuery<T> query, Expression<Func<T, long>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<long?> SumAsync(IQuery<T> query, Expression<Func<T, long?>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<float> SumAsync(IQuery<T> query, Expression<Func<T, float>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<float?> SumAsync(IQuery<T> query, Expression<Func<T, float?>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<double> SumAsync(IQuery<T> query, Expression<Func<T, double>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<double?> SumAsync(IQuery<T> query, Expression<Func<T, double?>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal> AverageAsync(IQuery<T> query, Expression<Func<T, decimal>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal?> AverageAsync(IQuery<T> query, Expression<Func<T, decimal?>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<float> AverageAsync(IQuery<T> query, Expression<Func<T, float>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<float?> AverageAsync(IQuery<T> query, Expression<Func<T, float?>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<double> AverageAsync(IQuery<T> query, Expression<Func<T, double>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<double?> AverageAsync(IQuery<T> query, Expression<Func<T, double?>> selector, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
 }
