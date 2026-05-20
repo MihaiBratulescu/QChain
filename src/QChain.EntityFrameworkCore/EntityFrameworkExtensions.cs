08cb8bf4e6f747a78413c8899a995f5d10562b9e
Bratulescu Mihai <mihaibratulescu@outlook.com>
2026-05-20 20:14:26 +0300

    sync extensions

--- src/QChain.EntityFrameworkCore/EntityFrameworkExtensions.cs
+++ src/QChain.EntityFrameworkCore/EntityFrameworkExtensions.cs
@@ -1,158 +1,12 @@
 ﻿using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using QChain.Internal;
 
 namespace QChain;
 
 public static class EntityFrameworkExtensions
 {
-    extension<T>(IQuery<T> query)
-    {
-        #region Any/All
-        public Task<bool> AnyAsync(CancellationToken ct = default) =>
-            Query(query, q => q.AnyAsync(ct));
-        public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.AnyAsync(ct));
-        public Task<bool> AllAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, q => q.AllAsync(predicate, ct));
-        #endregion
-
-        #region Count/LongCount
-        public Task<int> CountAsync(CancellationToken ct = default) =>
-            Query(query, q => q.CountAsync(ct));
-        public Task<int> CountAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.CountAsync(ct));
-        public Task<long> LongCountAsync(CancellationToken ct = default) =>
-            Query(query, q => q.LongCountAsync(ct));
-        public Task<long> LongCountAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.LongCountAsync(ct));
-        #endregion
-
-        #region ElementAt/ElementAtOrDefault
-        public Task<T> ElementAtAsync(int index, CancellationToken ct = default) =>
-            query.AsQueryable().ElementAtAsync(index, ct);
-
-        public Task<T?> ElementAtOrDefaultAsync(int index, CancellationToken ct = default) =>
-            query.AsQueryable().ElementAtOrDefaultAsync(index, ct);
-        #endregion
-
-        #region First/FirstOrDefault
-        public Task<T> FirstAsync(CancellationToken ct = default) =>
-            Query(query, q => q.FirstAsync(ct));
-        public Task<T> FirstAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.FirstAsync(ct));
-        public Task<T?> FirstOrDefaultAsync(CancellationToken ct = default) =>
-            Query(query, q => q.FirstOrDefaultAsync(ct));
-        public Task<T?> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.FirstOrDefaultAsync(ct));
-        #endregion
-
-        #region Last/LastOrDefault
-        public Task<T> LastAsync(CancellationToken ct = default) =>
-            Query(query, q => q.LastAsync(ct));
-        public Task<T> LastAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.LastAsync(ct));
-        public Task<T?> LastOrDefaultAsync(CancellationToken ct = default) =>
-            Query(query, q => q.LastOrDefaultAsync(ct));
-        public Task<T?> LastOrDefaultAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.LastOrDefaultAsync(ct));
-        #endregion
-
-        #region Single/SingleOrDefault
-        public Task<T> SingleAsync(CancellationToken ct = default) =>
-            Query(query, q => q.SingleAsync(ct));
-        public Task<T> SingleAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.SingleAsync(ct));
-        public Task<T?> SingleOrDefaultAsync(CancellationToken ct = default) =>
-            Query(query, q => q.SingleOrDefaultAsync(ct));
-        public Task<T?> SingleOrDefaultAsync(Expression<Func<T, bool>> predicate, CancellationToken ct = default) =>
-            Query(query, predicate, q => q.SingleOrDefaultAsync(ct));
-        #endregion
-
-        #region Min/Max
-        public Task<T> MinAsync(CancellationToken ct = default) =>
-            Query(query, q => q.MinAsync(ct));
-        public Task<R> MinAsync<R>(Expression<Func<T, R>> selector, CancellationToken ct = default) =>
-            Query(query.Select(selector), q => q.MinAsync(ct));
-        public Task<T> MaxAsync(CancellationToken ct = default) =>
-            Query(query, q => q.MaxAsync(ct));
-        public Task<R> MaxAsync<R>(Expression<Func<T, R>> selector, CancellationToken ct = default) =>
-            Query(query.Select(selector), q => q.MaxAsync(ct));
-        #endregion
-
-        #region Sum
-        public Task<decimal> SumAsync(Expression<Func<T, decimal>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.SumAsync(selector, ct));
-        public Task<decimal?> SumAsync(Expression<Func<T, decimal?>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.SumAsync(selector, ct));
-
-        public Task<int> SumAsync(Expression<Func<T, int>> selector, CancellationToken ct = default) =>
-          Query(query, q => q.SumAsync(selector, ct));
-        public Task<int?> SumAsync(Expression<Func<T, int?>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.SumAsync(selector, ct));
-
-        public Task<long> SumAsync(Expression<Func<T, long>> selector, CancellationToken ct = default) =>
-          Query(query, q => q.SumAsync(selector, ct));
-        public Task<long?> SumAsync(Expression<Func<T, long?>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.SumAsync(selector, ct));
-
-        public Task<float> SumAsync(Expression<Func<T, float>> selector, CancellationToken ct = default) =>
-          Query(query, q => q.SumAsync(selector, ct));
-        public Task<float?> SumAsync(Expression<Func<T, float?>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.SumAsync(selector, ct));
-
-        public Task<double> SumAsync(Expression<Func<T, double>> selector, CancellationToken ct = default) =>
-          Query(query, q => q.SumAsync(selector, ct));
-        public Task<double?> SumAsync(Expression<Func<T, double?>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.SumAsync(selector, ct));
-        #endregion
-
-        #region Average
-        public Task<decimal> AverageAsync(Expression<Func<T, decimal>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.AverageAsync(selector, ct));
-        public Task<decimal?> AverageAsync(Expression<Func<T, decimal?>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.AverageAsync(selector, ct));
-
-        public Task<float> AverageAsync(Expression<Func<T, float>> selector, CancellationToken ct = default) =>
-          Query(query, q => q.AverageAsync(selector, ct));
-        public Task<float?> AverageAsync(Expression<Func<T, float?>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.AverageAsync(selector, ct));
-
-        public Task<double> AverageAsync(Expression<Func<T, double>> selector, CancellationToken ct = default) =>
-          Query(query, q => q.AverageAsync(selector, ct));
-        public Task<double?> AverageAsync(Expression<Func<T, double?>> selector, CancellationToken ct = default) =>
-            Query(query, q => q.AverageAsync(selector, ct));
-        #endregion
-
-        #region ToList/Array
-        public Task<T[]> ToArrayAsync(CancellationToken ct = default) => Query(query, q => q.ToArrayAsync(ct));
-        public Task<List<T>> ToListAsync(CancellationToken ct = default) => Query(query, q => q.ToListAsync(ct));
-        #endregion
-
-        public string ToQueryString() =>
-            query.AsQueryable().ToQueryString();
-
-        public Task<bool> ContainsAsync(T item, CancellationToken ct = default) =>
-            Query(query, q => q.ContainsAsync(item, ct));
-
-        #region Helpers
-        internal Task<R> Query<R>(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, Task<R>> executor) =>
-            executor(query.Where(predicate).AsQueryable());
-        internal Task<R> Query<R>(Func<IQueryable<T>, Task<R>> executor) =>
-            executor(query.AsQueryable());
-        #endregion
-    }
-
     extension<T>(IQuery<T> query) where T : class
     {
-        #region Single/Split
-        public IQuery<T> AsSingleQuery() =>
-            new Query<T>(query.AsQueryable().AsSingleQuery());
-
-        public IQuery<T> AsSplitQuery() =>
-            new Query<T>(query.AsQueryable().AsSplitQuery());
-        #endregion
-
         #region Tracking
         public IQuery<T> AsNoTracking() =>
             new Query<T>(query.AsQueryable().AsNoTracking());
@@ -164,9 +18,21 @@ public static class EntityFrameworkExtensions
             new Query<T>(query.AsQueryable().AsTracking());
         #endregion
 
+        #region Single/Split
+        public IQuery<T> AsSingleQuery() =>
+            new Query<T>(query.AsQueryable().AsSingleQuery());
+
+        public IQuery<T> AsSplitQuery() =>
+            new Query<T>(query.AsQueryable().AsSplitQuery());
+        #endregion
+
         public IQuery<T> Include<E>(Expression<Func<T, E>> include) =>
             new Query<T>(query.AsQueryable().Include(include));
     }
 
-    
+    extension<T>(IQuery<T> query)
+    {
+        public string ToQueryString() =>
+            query.AsQueryable().ToQueryString();
+    }
 }
---
+++ src/QChain.EntityFrameworkCore/SyncExecution.cs
@@ -0,0 +1,140 @@
+﻿using System.Linq.Expressions;
+
+namespace QChain;
+
+public static class SyncExecution
+{
+    extension<T>(IQuery<T> query)
+    {
+        #region Any/All
+        public bool Any() =>
+            Query(query, q => q.Any());
+        public bool Any(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.Any());
+        public bool All(Expression<Func<T, bool>> predicate) =>
+            Query(query, q => q.All(predicate));
+        #endregion
+
+        #region Count/LongCount
+        public int Count() =>
+            Query(query, q => q.Count());
+        public int Count(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.Count());
+        public long LongCount() =>
+            Query(query, q => q.LongCount());
+        public long LongCount(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.LongCount());
+        #endregion
+
+        #region ElementAt/ElementAtOrDefault
+        public T ElementAt(int index) =>
+            query.AsQueryable().ElementAt(index);
+
+        public T? ElementAtOrDefault(int index) =>
+            query.AsQueryable().ElementAtOrDefault(index);
+        #endregion
+
+        #region First/FirstOrDefault
+        public T First() =>
+            Query(query, q => q.First());
+        public T First(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.First());
+        public T? FirstOrDefault() =>
+            Query(query, q => q.FirstOrDefault());
+        public T? FirstOrDefault(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.FirstOrDefault());
+        #endregion
+
+        #region Last/LastOrDefault
+        public T Last() =>
+            Query(query, q => q.Last());
+        public T Last(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.Last());
+        public T? LastOrDefault() =>
+            Query(query, q => q.LastOrDefault());
+        public T? LastOrDefault(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.LastOrDefault());
+        #endregion
+
+        #region Single/SingleOrDefault
+        public T Single() =>
+            Query(query, q => q.Single());
+        public T Single(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.Single());
+        public T? SingleOrDefault() =>
+            Query(query, q => q.SingleOrDefault());
+        public T? SingleOrDefault(Expression<Func<T, bool>> predicate) =>
+            Query(query, predicate, q => q.SingleOrDefault());
+        #endregion
+
+        #region Min/Max
+        public T? Min() =>
+            Query(query, q => q.Min());
+        public R? Min<R>(Expression<Func<T, R>> selector) =>
+            Query(query.Select(selector), q => q.Min());
+        public T? Max() =>
+            Query(query, q => q.Max());
+        public R? Max<R>(Expression<Func<T, R>> selector) =>
+            Query(query.Select(selector), q => q.Max());
+        #endregion
+
+        #region Sum
+        public decimal Sum(Expression<Func<T, decimal>> selector) =>
+            Query(query, q => q.Sum(selector));
+        public decimal? Sum(Expression<Func<T, decimal?>> selector) =>
+            Query(query, q => q.Sum(selector));
+
+        public int Sum(Expression<Func<T, int>> selector) =>
+          Query(query, q => q.Sum(selector));
+        public int? Sum(Expression<Func<T, int?>> selector) =>
+            Query(query, q => q.Sum(selector));
+
+        public long Sum(Expression<Func<T, long>> selector) =>
+          Query(query, q => q.Sum(selector));
+        public long? Sum(Expression<Func<T, long?>> selector) =>
+            Query(query, q => q.Sum(selector));
+
+        public float Sum(Expression<Func<T, float>> selector) =>
+          Query(query, q => q.Sum(selector));
+        public float? Sum(Expression<Func<T, float?>> selector) =>
+            Query(query, q => q.Sum(selector));
+
+        public double Sum(Expression<Func<T, double>> selector) =>
+          Query(query, q => q.Sum(selector));
+        public double? Sum(Expression<Func<T, double?>> selector) =>
+            Query(query, q => q.Sum(selector));
+        #endregion
+
+        #region Average
+        public decimal Average(Expression<Func<T, decimal>> selector) =>
+            Query(query, q => q.Average(selector));
+        public decimal? Average(Expression<Func<T, decimal?>> selector) =>
+            Query(query, q => q.Average(selector));
+
+        public float Average(Expression<Func<T, float>> selector) =>
+          Query(query, q => q.Average(selector));
+        public float? Average(Expression<Func<T, float?>> selector) =>
+            Query(query, q => q.Average(selector));
+
+        public double Average(Expression<Func<T, double>> selector) =>
+          Query(query, q => q.Average(selector));
+        public double? Average(Expression<Func<T, double?>> selector) =>
+            Query(query, q => q.Average(selector));
+        #endregion
+
+        #region ToList/Array
+        public T[] ToArray() => Query(query, q => q.ToArray());
+        public List<T> ToList() => Query(query, q => q.ToList());
+        #endregion
+
+        public bool Contains(T item) =>
+            Query(query, q => q.Contains(item));
+
+        #region Helpers
+        internal R Query<R>(Expression<Func<T, bool>> predicate, Func<IQueryable<T>, R> executor) =>
+            executor(query.Where(predicate).AsQueryable());
+        internal R Query<R>(Func<IQueryable<T>, R> executor) =>
+            executor(query.AsQueryable());
+        #endregion
+    }
+}
