1da753e22655c2adcfa45899789794dbcf454c4d
Bratulescu Mihai <mihaibratulescu@outlook.com>
2026-09-19 03:30:48 +0300

    cleanup

--- src/PCompose/PredicateCompiler.cs
+++ src/PCompose/PredicateCompiler.cs
@@ -1,28 +1,22 @@
-﻿using PCompose.Internal;
+﻿
+using PCompose.Internal;
 using PCompose.Visitors;
 using System.Linq.Expressions;
-using System.Reflection;
 
 namespace PCompose;
 
 public static class PredicateCompiler
 {
-    extension(Predicate predicate)
+    extension<T>(Func<T, Predicate> predicate)
     {
-        public Expression<Func<T, bool>> Compile<T>()
+        public Expression<Func<T, bool>> Compile()
         {
             var parameter = Expression.Parameter(typeof(T), "x");
 
-            var body = CompilePredicate(predicate, parameter);
+            var body = CompilePredicate(predicate(default(T)!), parameter);
 
             return Expression.Lambda<Func<T, bool>>(body, parameter);
         }
-    }
-    
-    extension<T>(Func<T, Predicate> predicate)
-    {
-        public Expression<Func<T, bool>> Compile() =>
-            predicate(default(T)!).Compile<T>();
     }
 
     private static Expression CompilePredicate(Predicate predicate, ParameterExpression root)
@@ -45,57 +39,17 @@ public static class PredicateCompiler
     }
 
     private static Expression ApplyCondition(LambdaExpression condition, ParameterExpression root)
-        => condition.Parameters.Count == 1
-            ? ApplyConditionByType(condition, root)
-            : ApplyConditionByPosition(condition, root);
-
-    private static Expression ApplyConditionByType(LambdaExpression condition, ParameterExpression root)
     {
+        if (condition.Parameters.Count != 1)
+            throw new InvalidOperationException(
+                $"Invalid number of arguments: expected 1, but received {condition.Parameters.Count}.");
+
         var parameter = condition.Parameters[0];
         if (parameter.Type == root.Type)
             return ReplaceExpressionVisitor.Replace(condition.Body, parameter, root);
 
-        foreach (var member in GetMembers(root.Type))
-        {
-            var target = Expression.MakeMemberAccess(root, member);
-            if (target.Type == parameter.Type)
-                return ReplaceExpressionVisitor.Replace(condition.Body, parameter, target);
-        }
-
         throw new InvalidOperationException(
-            $"Invalid argument type: expected a member type of {root.Type.Name}, but received {parameter.Type.Name}.");
+            $"Invalid argument type: expected {root.Type.Name}, but received {parameter.Type.Name}.");
     }
 
-    private static Expression ApplyConditionByPosition(LambdaExpression condition, ParameterExpression root)
-    {
-        var members = GetMembers(root.Type);
-
-        if (condition.Parameters.Count > members.Length)
-            throw new InvalidOperationException(
-                $"Invalid number of arguments: expected at most {members.Length}, but received {condition.Parameters.Count}.");
-
-        var replacements = new Dictionary<Expression, Expression>();
-
-        for (var index = 0; index < condition.Parameters.Count; index++)
-        {
-            var parameter = condition.Parameters[index];
-            var target = Expression.MakeMemberAccess(root, members[index]);
-
-            if (target.Type != parameter.Type)
-                throw new InvalidOperationException(
-                    $"Invalid argument type at position {index + 1}: expected {target.Type.Name}, but received {parameter.Type.Name}.");
-
-            replacements.Add(parameter, target);
-        }
-
-        return ReplaceExpressionVisitor.ReplaceMany(condition.Body, replacements);
-    }
-
-    private static MemberInfo[] GetMembers(Type type) =>
-        type.GetFields(BindingFlags.Public | BindingFlags.Instance)
-            .OrderBy(member => member.MetadataToken)
-            .Cast<MemberInfo>()
-            .Concat(type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .OrderBy(member => member.MetadataToken))
-            .ToArray();
 }
